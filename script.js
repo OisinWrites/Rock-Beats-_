8e6d5909349f6de79011a67a8b45fa4165bc8e28
OisinWrites <oisin.banville@gmail.com>
2022-08-19 22:39:23 +0000

    creat const for player choices in script file

---
+++ script.js
@@ -0,0 +1,28 @@
+const choices = document.querySelectorAll("[choice]")
+const CHOICES = [
+    {
+        name:'rock',
+        img:<img src="assets/images/rock.png"></img>,
+        beats:'scissors lizard'
+    },
+    {
+        name:'paper',
+        img:<img src="assets/images/paper.png"></img>,
+        beats:'rock spock'
+    },
+    {
+        name:'scissors',
+        img:<img src="assets/images/scissor.png"></img>,
+        beats:'paper lizard'
+    },
+    {
+        name:'lizard',
+        img:<img src="assets/images/lizard.png"></img>,
+        beats:'paper spock'
+    },
+    {
+        name:'spock',
+        img:<img class="button" src="assets/images/spock.png"></img>,
+        beats:'rock scissors'
+    }
+]
