ed1529111959b04e8c8a58742686ce9a4b59a6a4
OisinWrites <oisin.banville@gmail.com>
2022-08-20 09:18:20 +0000

    change img src in js from const to var

--- script.js
+++ script.js
@@ -2,27 +2,37 @@ const choices = document.querySelectorAll("[choice]")
 const CHOICES = [
     {
         name:'rock',
-        img:<img src="assets/images/rock.png"></img>,
+        img:'rockImg',
         beats:'scissors lizard'
     },
     {
         name:'paper',
-        img:<img src="assets/images/paper.png"></img>,
+        img:"paperImg",
         beats:'rock spock'
     },
     {
         name:'scissors',
-        img:<img src="assets/images/scissor.png"></img>,
+        img:"scissorsImg",
         beats:'paper lizard'
     },
     {
         name:'lizard',
-        img:<img src="assets/images/lizard.png"></img>,
+        img:'lizardImg',
         beats:'paper spock'
     },
     {
         name:'spock',
-        img:<img class="button" src="assets/images/spock.png"></img>,
+        img:'spockImg',
         beats:'rock scissors'
     }
 ]
+var img = rockImg();
+img.src = "assets/images/rock.png";
+var img = paperImg();
+img.src = "assets/images/paper.png";
+var img = scissorsImg();
+img.src = "assets/images/scissor.png";
+var img = lizardImg();
+img.src = "assets/images/lizard.png";
+var img = spockImg();
+img.src = "assets/images/spock.png";
