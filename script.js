cc34673741c8af92aa80ef07a056e542f29b79f7
OisinWrites <oisin.banville@gmail.com>
2022-08-21 09:33:02 +0000

    create button functionality on 'rock' img with console log of current time to test

---
+++ script.js
@@ -0,0 +1,7 @@
+const rockButton = document.querySelector('[rockButton]');
+   
+rockButton.addEventListener('click', e => {
+        const now = new Date(); 
+            console.log(now);
+    })
+
