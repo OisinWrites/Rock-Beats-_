2589998902458c9a46bb3a5709abfebe7b970a44
OisinWrites <oisin.banville@gmail.com>
2022-08-24 08:34:13 +0000

    remove unused links from header and add styling to reset buttons and responsivity to 'battlefield'

--- assets/javaScript/script.js
+++ assets/javaScript/script.js
@@ -80,9 +80,9 @@ function imagesToLog(playerChoice, resultMessage, cpuChoice) {
 //When any of the conditions are met it will call on our resetButton function, and on
 //our countersToScreen function so that the reset to 0 counters will refresh.
 function gameEnd() {
-    const winReset = '<div id="resetButton" class="buttonreset">*Congratulations!!*<br><p class="resetstyling">reset</p></div>';
-    const tiesReset = '<div id="resetButton" class="buttonreset">Try Again<br><p class="resetstyling">reset</p></div>';
-    const loseReset = '<div id="resetButton" class="buttonreset">You Ambitious Fool!<br><p class="resetstyling">reset</p></div>';
+    const winReset = '<div id="resetButton" class="buttonresetcongrat">*Congratulations!!*<br><p class="resetstyling">reset</p></div>';
+    const tiesReset = '<div id="resetButton" class="buttonresetry">Try Again<br><p class="resetstyling">reset</p></div>';
+    const loseReset = '<div id="resetButton" class="buttonresetfail">You Ambitious Fool!<br><p class="resetstyling">reset</p></div>';
     if(winCounter === 3 || loseCounter === 3 || numberClicks === 5){
         if(winCounter > loseCounter) {
             document.getElementById('defeat').innerHTML = '<img class="avatar stickman" src="assets/images/victory.png">';
